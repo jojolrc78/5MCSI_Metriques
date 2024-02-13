80f8b73511d05ec43c6d6f8a318f1ff19b4f77c8
jojolrc78 <148208020+jojolrc78@users.noreply.github.com>
2024-02-13 15:26:12 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,9 +4,40 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-import requests
                                                                                                                                        
-app = Flask(__name__) 
+app = Flask(__name__)                                                                                                                  
+
+# Route pour extraire les minutes d'une date
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
+
+# Route pour extraire et afficher les commits par minute
+@app.route('/commits/')
+def commits():
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    response = requests.get(url)
+    commits_data = response.json()
+    
+    # Initialisation du dictionnaire pour stocker le nombre de commits par minute
+    commits_per_minute = {}
+    
+    # Parcourir les commits et compter le nombre de commits par minute
+    for commit in commits_data:
+        date_string = commit['commit']['author']['date']
+        minutes = extract_minutes(date_string).json['minutes']
+        if minutes in commits_per_minute:
+            commits_per_minute[minutes] += 1
+        else:
+            commits_per_minute[minutes] = 1
+    
+    # Préparer les données pour le graphique
+    minutes = list(commits_per_minute.keys())
+    commit_counts = list(commits_per_minute.values())
+    
+    return jsonify({'minutes': minutes, 'commit_counts': commit_counts})
   
 @app.route("/contact/")
 def Utilisateur():
