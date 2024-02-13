919561fb53b19559920738a6dbbe5d829d76daaa
jojolrc78 <148208020+jojolrc78@users.noreply.github.com>
2024-02-13 14:26:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ def meteo():
 @app.route("/contact/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
-  
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Comm2
